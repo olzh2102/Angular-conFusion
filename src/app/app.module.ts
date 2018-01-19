714ca85acd0df6069342ac4451b500d39cd353b6
olzhas2102 <olzhas2102@yahoo.com>
2018-01-19 22:37:09 +0600

    SPA part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -46,7 +47,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService ],
+  providers: [ DishService,
+      PromotionService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
