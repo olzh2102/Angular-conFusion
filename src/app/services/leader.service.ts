ec81a4d21f6a1ec10140e18e484ae3f8997edc75
olzhas2102 <olzhas2102@yahoo.com>
2018-01-28 00:48:57 +0600

    Promise Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -8,16 +8,16 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Leader[] {
-    return LEADERS;
+  getLeaders(): Promise<Leader[]> {
+    return Promise.resolve(LEADERS);
   }
 
-  getLeader(id: number): Leader{
-    return LEADERS.filter((leader) => (leader.id === id))[0];
+  getLeader(id: number): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
   }
 
-  getFeaturedDLeader(): Leader{
-    return LEADERS.filter((leader) => (leader.featured))[0];
+  getFeaturedDLeader(): Promise<Leader> {
+    return Promise.resolve(LEADERS.filter((leader) => (leader.featured))[0]);
   }
 
 }
